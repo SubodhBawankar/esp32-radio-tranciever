3f647da95ea4410518f8f6854739c1e986e821f3
SubodhBawankar <aryanbawankar14@gmail.com>
2022-09-01 20:31:49 +0530

    Added Oled header file

--- 3_oled/main/potentiometer_oled.c
+++ 3_oled/main/potentiometer_oled.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/adc.h"
 #include "esp_log.h"
+#include "driver/adc.h"
 
-static const char* TAG = "POTENTIOMETER ";
+#include "oled.h"
+#include "u8g2.h"
+
+static const char* TAG = "Poten+Oled";
 
 #define POTEN_PIN 35
-// Potentiometer Output Pin is connected to 32 Pin of ESP32
+// Potentiometer Output Pin is connected to 35 Pin of ESP32
 
 void config_adc(){
 
-    adc1_config_width(ADC1_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC1_CHANNEL_7, ADC1_ATTEN_DB_11);
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_DB_11);
     // ADC_CHANNEL_7 refers to 35 pin on 
     // esp32 where we have connected our potentiometer
     ESP_LOGI(TAG, "Config Done");
@@ -21,9 +25,14 @@ void config_adc(){
 
 void character_adc(){
 
-    uint32_t reading;
+    int reading;
     reading = adc1_get_raw(ADC1_CHANNEL_7);
     ESP_LOGI(TAG, "Reading %d", reading);
+
+    u8g2_t u8g2_structure;
+    led_config(u8g2_structure);
+    printvoltage(u8g2_structure, reading);
+
 }
 
 
@@ -31,8 +40,8 @@ void character_adc(){
 void app_main(void){
 
     config_adc();
-    
     while (1){
         character_adc();
     }
 }
+
---
+++ components/src/oled.c
@@ -0,0 +1,40 @@
+#include "oled.h"
+
+esp_err_t led_config(u8g2_t config_u8g2){
+
+    u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
+	u8g2_esp32_hal.sda = OLED_SDA;
+	u8g2_esp32_hal.scl = OLED_SCL;
+	u8g2_esp32_hal_init(u8g2_esp32_hal);
+
+    u8g2_Setup_ssd1306_i2c_128x32_univision_f(
+        &config_u8g2, 
+        U8G2_R0, 
+        u8g2_esp32_i2c_byte_cb, 
+        u8g2_esp32_gpio_and_delay_cb);
+	
+    //u8x8_SetI2CAddress(&u8g2->u8x8, 0x78);
+
+    u8g2_InitDisplay(&config_u8g2);// send init sequence to the display, 
+    //display is in sleep mode after this
+
+    u8g2_SetPowerSave(&config_u8g2, 0); // wake up
+    
+    u8g2_ClearBuffer(&config_u8g2);
+	u8g2_SendBuffer(&config_u8g2);
+	ESP_LOGI("TAG_OLED", "Configured OLED Display");
+
+    return ESP_OK;
+}
+
+esp_err_t printvoltage(u8g2_t config_u8g2, int reading){    
+    char volt_str[20];
+
+    u8g2_SetFont(&config_u8g2, u8g2_font_ncenB10_tr);
+
+    sprintf(volt_str, "Voltage : %d", reading);
+    u8g2_ClearBuffer(&config_u8g2);
+    u8g2_DrawStr(&config_u8g2, 2, 15, volt_str);
+    u8g2_SendBuffer(&config_u8g2);
+    return ESP_OK;
+}
