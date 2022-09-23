d477df959be059692989284f32420c9b6d425c11
Janhavi Deshpande <janhavideshpande18@gmail.com>
2022-09-23 23:55:01 +0530

    Spi Communication

---
+++ SPI_Task/main/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include <driver/spi_master.h>
+#include "driver/spi_common.h"
+
+#include <driver/gpio.h>
+#include "esp_log.h"
+
+#include "SPI.h"
+ void app main()
+ {
+    esp_err_t spi_config();
+    esp_err_t transfer_data();
+ }
