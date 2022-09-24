d5dc3e539dcc12184a52416d4e30ef90f30c7a17
Janhavi Deshpande <janhavideshpande18@gmail.com>
2022-09-24 20:27:07 +0530

    Made Changes

---
+++ SPI_Task_T/main/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+
+#include "spi.h"
+
+void Transmittor()
+{
+    ESP_LOGI(pcTaskGetName(0), "Start");
+	uint8_t pl = sizeof(mydata.value);
+	uint8_t ch = 90;
+	Config_nRF(ch, pl);
+
+	esp_err_t ret = set_T_Add((uint8_t *)"FGHIJ");
+	if (ret != ESP_OK) 
+    {
+		ESP_LOGE(pcTaskGetName(0), "ERROR:nrf24l01 not installed");
+		while(1)
+        { 
+         vTaskDelay(1); 
+        }
+	}
+    
+    while(1)
+    {
+		Send(mydata.value);
+		vTaskDelay(1);
+
+		ESP_LOGI(pcTaskGetName(0), "Wait for sending.....");
+
+		/*if (Sending())
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+        {
+			ESP_LOGI(pcTaskGetName(0),"Send success:%lu", mydata.now_time);
+		} 
+        else
+        {
+			ESP_LOGW(pcTaskGetName(0),"Send fail:");
+		}*/
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+	}
+}
+ 
+void app_main()
+{
+    xTaskCreate(transmitter, "TRANSMITTER", 1024*3, NULL, 2, NULL);
+}
